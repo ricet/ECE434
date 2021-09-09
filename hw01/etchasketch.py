8d8e6e3c86c43c3f20822cfbbf5040d3b1184328
Tiarnan Rice <ricet@rose-hulman.edu>
2021-09-09 01:09:45 -0400

    draws board outline

--- hw01/etchasketch.py
+++ hw01/etchasketch.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 
+import curses
 from curses import wrapper
+import sys
 
 def main(stdscr):
     # Clear screen
     stdscr.clear()
 
-    # This raises ZeroDivisionError when i == 10.
-    for i in range(0, 5):
-        v = i-10
-        stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10/v))
-
+    # Draw board
+    width = int(sys.argv[1])
+    height = int(sys.argv[2])
+    for i in range(0, width*2, 2):
+        stdscr.addch(0, i, "*")
+        stdscr.addch(height, i, "*")
+    for j in range(0, height):
+        stdscr.addch(j, 0, "*")
+        stdscr.addch(j, width*2, "*")
     stdscr.refresh()
     stdscr.getkey()
 
+print(str(sys.argv[1]))
 wrapper(main)
