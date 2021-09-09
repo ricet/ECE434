e15e5223c14a794b1a00b9be6c507d9869b4971b
Tiarnan Rice <ricet@rose-hulman.edu>
2021-09-09 13:00:43 -0400

    commented code

--- hw01/etchasketch.py
+++ hw01/etchasketch.py
@@ -9,15 +9,19 @@ def main(stdscr):
     # Clear screen
     stdscr.clear()
     stdscr.nodelay(False)
-    # Draw board
+    # Read arguments and draw board
     width = int(sys.argv[1])
     height = int(sys.argv[2])
     drawboard(stdscr, width, height)
 
+    # Cursor starting coordinates
     cursorx = 2
     cursory = 1
+
     while True:
+        # Wait for user input
         c = stdscr.getch()
+        # Adjust cursor coordinates based off key press
         if c == curses.KEY_DOWN:
             cursory = cursory + 1
         if c == curses.KEY_UP:
@@ -26,20 +30,25 @@ def main(stdscr):
             cursorx = cursorx - 2
         if c == curses.KEY_RIGHT:
             cursorx = cursorx + 2
+        # Break loop and quit if escape is pressed
         if c == 27:
             break
+        # Keep cursor within board perimeter
         if cursorx < 2: cursorx = 2
         if cursory < 1: cursory = 1
         if cursorx >= width*2: cursorx = width*2
         if cursory > height: cursory = height
+        # Update cursor location with new coordinates and draw an X 
         stdscr.move(cursory, cursorx)
         stdscr.addch(chr(88))
-
+        
+        # If space is pressed clear screen and redraw board
         if c == 32:
            stdscr.clear()
            drawboard(stdscr, width, height)
 
 
+# Draw board function, draws asterisks around the perimeter of the drawing area
 def drawboard(stdscr, width, height):
     for i in range(0, width*2+2, 2):
         stdscr.addch(0, i, "*")
@@ -47,6 +56,8 @@ def drawboard(stdscr, width, height):
     for j in range(0, height+1):
         stdscr.addch(j, 0, "*")
         stdscr.addch(j, width*2+2, "*")
-
+    # Put instructions on screen
     stdscr.addstr(height+3, 0, "Use the arrow keys to draw, space to clear the screen, and escape to quit")
+
+# Curses function to call main, handles curses things
 wrapper(main)
