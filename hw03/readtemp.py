bf990d593b98e77391f37978109737196a72f543
Tiarnan Rice <ricet@rose-hulman.edu>
2021-09-23 20:27:03 +0000

    temp stuff

--- hw03/readtemp.py
+++ hw03/readtemp.py
@@ -8,11 +8,9 @@ addressA = 0x48
 addressB = 0x4a
 
 while True:
-    #tempA = round(9/5 * bus.read_byte_data(addressA, 0) + 32, 1)
-    tempA = 20
+    tempA = round(9/5 * bus.read_byte_data(addressA, 0) + 32, 1)
     tempB = round(9/5 * bus.read_byte_data(addressB, 0) + 32, 1)
-    #errorA = (bus.read_byte_data(addressA, 1) >> 7) ^ 1
-    errorA = 69
+    errorA = (bus.read_byte_data(addressA, 1) >> 7) ^ 1
     errorB = (bus.read_byte_data(addressB, 1) >> 7) ^ 1
     print("Temp A is: " + str(tempA) + "°F   Alert A: " + str(errorA) + "    Temp B is: " + str(tempB) + "°F   Alert B: " + str(errorB))
     time.sleep(1)
--- hw03/settemp.py
+++ hw03/settemp.py
@@ -18,16 +18,21 @@ GPIO.setup(alertBpin, GPIO.IN)
 sethigh = int(sys.argv[1])
 setlow = int(sys.argv[2])
 
-#bus.write_byte_data(addressA, 3, setA)
-bus.write_byte_data(addressB, 2, 0x1B)
-bus.write_byte_data(addressB, 3, 0x1C)
+bus.write_byte_data(addressA, 2, setlow)
+bus.write_byte_data(addressA, 3, sethigh)
+bus.write_byte_data(addressB, 2, setlow)
+bus.write_byte_data(addressB, 3, sethigh)
 
 map = {alertApin: addressA, alertBpin: addressB}
 
 def print_temp(alertpin):
-    tempB = round(bus.read_byte_data(map[alertpin], 0), 1)
-    print("Alert: Temp B is " + str(tempB))
+    temp = round(bus.read_byte_data(map[alertpin], 0), 1)
+    if alertpin == alertApin:
+        print("Alert: Temp A is " + str(temp))
+    elif alertpin == alertBpin:
+        print("Alert: Temp B is " + str(temp))
     
+GPIO.add_event_detect(alertApin, GPIO.FALLING, callback=print_temp)
 GPIO.add_event_detect(alertBpin, GPIO.FALLING, callback=print_temp)
 
 while True:
