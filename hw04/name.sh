b5871a065c04e4d33828544ae62a6071c952fdf0
Tiarnan Rice <ricet@rose-hulman.edu>
2021-10-01 20:49:03 +0000

    stuff

--- hw04/name.sh
+++ hw04/name.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 SIZE=320x240
-TMP_FILE=frame.png
+rm -rf /tmp/frame.png
+TMP_FILE=/tmp/frame.png
 
-convert rice.png -stroke '#000C' -strokewidth 2 -annotate 0 'TIARNAN RICE' \
-          -stroke  none   -fill white    -annotate 0 'TIARNAN RICE' \
-          $TMP_FILE
+convert rice.png -size $SIZE $TMP_FILE
 
-sudo fbi -noverbose -T 1 $TMP_FILE
+sudo fbi -noverbose -T 1 -a $TMP_FILE
