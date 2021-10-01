1333529c5329c1f0c1e755d7407873dc0468b588
Tiarnan Rice <ricet@rose-hulman.edu>
2021-10-01 23:29:43 +0000

    stuff

--- hw04/name.sh
+++ hw04/name.sh
@@ -4,7 +4,7 @@ SIZE=320x240
 TMP_FILE=/tmp/frame.png
 
 convert rice.png -gravity Center \
-          -stroke '#000C' -strokewidth 10 -annotate 10 'Tiarnan Rice' \
+          -stroke '#000C' -pointsize 40 -font Candice -strokewidth 20 -annotate 10 'Tiarnan Rice' \
           -stroke  none   -fill white    -annotate 10 'Tiarnan Rice' \
           -size $SIZE $TMP_FILE
 
