1209870bb51666da0d026620e11d6a83d93fcbe0
Tiarnan Rice <ricet@rose-hulman.edu>
2021-10-05 20:27:07 +0000

    kernel temp works

--- hw04/kerneltemp.sh
+++ hw04/kerneltemp.sh
@@ -3,14 +3,14 @@
 cd /sys/class/i2c-adapter/i2c-2
 
 # If the device doesnt exist then create it
-if[! -d "2-0049"]
+if [ ! -d "2-0049" ]
 then
     echo tmp101 0x49 > new_device
 fi
 cd 2-0049/hwmon/hwmon0
 
 # Break temp into whole number and decimals
-temp=$(cat temp)
+temp=$(cat temp1_input)
 temp1=$((temp/1000))
 temp2=$((temp%1000))
 
